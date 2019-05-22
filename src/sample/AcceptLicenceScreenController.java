dc34d116e6f7cbf1db2e1fc010f8b3d718e42c65
Dorel Barbu <dorelbarbu96@gmail.com>
2019-05-23 00:10:27 +0300

    Added SelectImage screen

--- src/sample/AcceptLicenceScreenController.java
+++ src/sample/AcceptLicenceScreenController.java
@@ -1,8 +1,14 @@
 package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+
+
+import java.io.IOException;
 
 public class AcceptLicenceScreenController {
     @FXML
@@ -11,7 +17,21 @@ public class AcceptLicenceScreenController {
     @FXML
     private void handlePressCancel(ActionEvent event) {
         System.out.println("Cancel button pressed");
-        Stage stage = (Stage) cancelButton.getScene().getWindow();
-        stage.close();
+        /*Close the current scene*/
+//        Stage stage = (Stage) cancelButton.getScene().getWindow();
+//        stage.hide();
+        /*Open the select image screen*/
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("SelectImage.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+            Stage stage = new Stage();
+            stage.setTitle("New Window");
+            stage.setScene(scene);
+            stage.show();
+        } catch(IOException e) {
+            System.out.println("ERROR: Cannot open the SelectImage screen");
+        }
+
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("AcceptLicenceScreen.fxml"));
         primaryStage.setTitle("Licence Agreement");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setScene(new Scene(root, 600, 300));
         primaryStage.show();
     }
 
